f1a8f69d1c1c821ab995003948b5c1276126fe57
Arle89 <anarhyarlekin@gmail.com>
2021-07-04 10:59:54 +0300

    making method to interact with vehicle

--- parking.py
+++ parking.py
@@ -15,10 +15,24 @@ class Parking_place():
 class Moto(Parking_place):
     def __init__(self):
         self.size = size = 1
-        self.park_place = park_place = Parking_place.display_parking()
+        self.park_place = park_place = Parking_place.matrix
     
-    def parking():
-        for i in 
+    def parking(self):
+        for i in self.park_place:
+            for j in i:
+                if j == "empty":
+                    print(i.index(j))
+                    j = "moto"
+                else:
+                    print("Parking is busy")
+        print(Parking_place.display_parking)
+
+class Avto(Parking_place):
+    def __init__(self):
+        self.size = size = 1
+
 
 x = Parking_place()
 x.display_parking()
+y = Moto()
+y.parking
